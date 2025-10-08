a95cdf0ac4020183a75397daf695156b8197071d
伟 <wangwei1834210@gmail.com>
2025-10-08 05:07:56 +0100

    Create 1486. 数组异或操作.java

---
+++ 力扣/简单/1486. 数组异或操作.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int ans = 0;
+        
+        while (n > 0) {
+            ans ^= start;
+            start += 2;
+            n --;
+        }
+        
+        return ans;
+    }
+}
